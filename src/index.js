02e94d76fe58dc1540e94eec116138adf9fea80f
lanzhengxian <lanzhengxian@gmail.com>
2016-09-05 14:21:23 +0800

    -mes6 module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import Point from './App';
 
-render(<App />, document.getElementById('root'));
+var pnt = new Point(1,2);
+console.log(pnt.toString());
