b8f46f970c63f819dd22d9db162ca09f82ac42c4
lanzhengxian <lanzhengxian@gmail.com>
2016-09-15 12:08:12 +0800

    add react form

--- src/Account.js
+++ src/Account.js
@@ -1,12 +1,34 @@
 import React, {
   Component
 } from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+import TextField from 'material-ui/lib/text-field';
+/* 注意这里引入card 是 card/card */
+import Card from 'material-ui/lib/card/card';
+
 
 class Account extends Component {
-  render() {
+
+  constructor(props){
+    super(props);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  _handleSubmit(e){
+    e.preventDefault();
+    const account = this.refs.account.getValue();
+    console.log(`account => ${account}`);
+  }
+
+  render() {  
     return (
       <div className="account">
-        <div className="slogan">Welcome to Account</div>
+        <Card className="content">
+        <form onSubmit={this._handleSubmit} >
+          <TextField hintText="Your Github Account" ref="account"/>
+          <FlatButton primary={true} label="Search Github" type="submit"/>
+        </form>
+        </Card>
       </div>
     );
   }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,6 +9,7 @@ module.exports = {
     './src/index'
   ],
   output: {
+    /* output其实可以随便写，因为在dev环境内，不会真的输入文件到硬盘，而是在内存中 */
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
